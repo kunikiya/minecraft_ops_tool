81af05d906117102a7946f14a8c6f576b4de24e1
kunikiya <kunikiya@live.jp>
2022-02-24 13:54:16 +0900

    add backup & start script

---
+++ mc_backup.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+tar cfvz /home/kunikiya/mc_backup/minecraft_`date +%Y%m%d-%H%M%S`.tar.gz /home/kunikiya/minecraft/
+
+
---
+++ mc_start.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# minecraft_server.1.16.4
+java -Xmx3584M -Xms2048M -jar ./server.jar nogui
+
